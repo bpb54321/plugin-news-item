5f2d2773ae80b9eb9c632d7a17ee449cbf355ee3
Brian Blosser <brian@slamagency.com>
2016-12-19 22:01:43 -0600

    Spitting out the custom fields in archive-news-item.

--- archive-news-item.php
+++ archive-news-item.php
@@ -1,1 +1,23 @@
-<h1>I am the News Item archive!</h1>
+
+
+<?php if ( have_posts() ) : ?>
+  <?php while ( have_posts() ) : ?>
+    <?php the_post(); ?>
+    <?php
+      //Get data for each news item
+      $link = get_field('link');
+      $description = get_field('description');
+      $date = get_field('date');
+    ?>
+
+    <a href="<?= $link ?>" class="news-item__link">
+      <h2 class="news-item__title"><?php the_title(); ?></h2>
+    </a>
+    <p class="news-item__date"><?= $date ?></p>
+    <img src="" alt="" class="news-item__image">
+    <p class="news-item__description"><?= $description ?></p>
+
+  <?php endwhile; ?>
+<?php else : ?>
+  <h1>There are currently no news items to display!</h1>
+<?php endif; ?>
