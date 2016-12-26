38c45202e162553c21b860ab24ddc7e369aa4f7f
Brian <brian@toky.com>
2016-12-26 15:12:58 -0600

    Successfully enqueued the plugin stylesheet.

--- archive-news-item.php
+++ archive-news-item.php
@@ -1,28 +1,31 @@
 <?php get_header(); ?>
 
 <div id="fixed-layout-content-area">
-  <?php if ( have_posts() ) : ?>
-    <?php while ( have_posts() ) : ?>
-      <?php the_post(); ?>
-      <?php
-        //Get data for each news item
-        $link = get_field('link');
-        $description = get_field('description');
-        $date = get_field('date');
-      ?>
+  <div class="news-archive">
+    <h1>NEWS</h1>
+    <?php if ( have_posts() ) : ?>
+      <?php while ( have_posts() ) : ?>
+        <?php the_post(); ?>
+        <?php
+          //Get data for each news item
+          $link = get_field('link');
+          $description = get_field('description');
+          $date = get_field('date');
+        ?>
 
-      <article class="news-item">
-        <a href="<?= $link ?>" class="news-item__link">
-          <h2 class="news-item__title"><?php the_title(); ?></h2>
-        </a>
-        <p class="news-item__date"><?= $date ?></p>
-        <img src="" alt="" class="news-item__image">
-        <p class="news-item__description"><?= $description ?></p>
-      </article>
+        <article class="news-archive__news-item">
+          <a href="<?= $link ?>" class="news-item__link">
+            <h2 class="news-archive__title"><?php the_title(); ?></h2>
+          </a>
+          <p class="news-archive__date"><?= $date ?></p>
+          <img src="" alt="" class="news-item__image">
+          <p class="news-archive__description"><?= $description ?></p>
+        </article>
 
-    <?php endwhile; ?>
-  <?php else : ?>
-    <h1>There are currently no news items to display!</h1>
-  <?php endif; ?>
+      <?php endwhile; ?>
+    <?php else : ?>
+      <h1>There are currently no news items to display!</h1>
+    <?php endif; ?>
+  </div>
 </div>
 <?php get_footer(); ?>
