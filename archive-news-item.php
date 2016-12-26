f07db13fd41ef35a28213ff2ce7627eb5ca87416
Brian <brian@toky.com>
2016-12-26 14:26:53 -0600

    Pulled in the header and footer on archive-news-item.php.

--- archive-news-item.php
+++ archive-news-item.php
@@ -1,23 +1,28 @@
+<?php get_header(); ?>
 
+<div id="fixed-layout-content-area">
+  <?php if ( have_posts() ) : ?>
+    <?php while ( have_posts() ) : ?>
+      <?php the_post(); ?>
+      <?php
+        //Get data for each news item
+        $link = get_field('link');
+        $description = get_field('description');
+        $date = get_field('date');
+      ?>
 
-<?php if ( have_posts() ) : ?>
-  <?php while ( have_posts() ) : ?>
-    <?php the_post(); ?>
-    <?php
-      //Get data for each news item
-      $link = get_field('link');
-      $description = get_field('description');
-      $date = get_field('date');
-    ?>
+      <article class="news-item">
+        <a href="<?= $link ?>" class="news-item__link">
+          <h2 class="news-item__title"><?php the_title(); ?></h2>
+        </a>
+        <p class="news-item__date"><?= $date ?></p>
+        <img src="" alt="" class="news-item__image">
+        <p class="news-item__description"><?= $description ?></p>
+      </article>
 
-    <a href="<?= $link ?>" class="news-item__link">
-      <h2 class="news-item__title"><?php the_title(); ?></h2>
-    </a>
-    <p class="news-item__date"><?= $date ?></p>
-    <img src="" alt="" class="news-item__image">
-    <p class="news-item__description"><?= $description ?></p>
-
-  <?php endwhile; ?>
-<?php else : ?>
-  <h1>There are currently no news items to display!</h1>
-<?php endif; ?>
+    <?php endwhile; ?>
+  <?php else : ?>
+    <h1>There are currently no news items to display!</h1>
+  <?php endif; ?>
+</div>
+<?php get_footer(); ?>
