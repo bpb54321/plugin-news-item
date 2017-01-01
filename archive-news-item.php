4e1f0555b3f32c359968f90cedcc925195ca5134
Brian <brian@toky.com>
2017-01-01 12:08:13 -0600

    Added div.scrollable.

--- archive-news-item.php
+++ archive-news-item.php
@@ -14,38 +14,40 @@
   }
 ?>
 <div id="fixed-layout-content-area">
-  <div class="news-archive">
-    <h1>NEWS</h1>
-    <?php if ( have_posts() ) : ?>
-      <?php while ( have_posts() ) : ?>
-        <?php the_post(); ?>
-        <?php
-          //Get data for each news item
-          $link = htmlentities( get_field('link') );
-          $description = htmlentities( get_field('description') );
-          $date_field = get_field('date');
-          $date = DateTime::createFromFormat('Ymd', $date_field);
-          $formatted_date = $date->format('F j, Y');
-          $featured_image_id = get_post_thumbnail_id();
-          $image_src_array = wp_get_attachment_image_src($featured_image_id, 'medium');
-        ?>
+  <div class="scrollable">
+    <div class="news-archive">
+      <h1>NEWS</h1>
+      <?php if ( have_posts() ) : ?>
+        <?php while ( have_posts() ) : ?>
+          <?php the_post(); ?>
+          <?php
+            //Get data for each news item
+            $link = htmlentities( get_field('link') );
+            $description = htmlentities( get_field('description') );
+            $date_field = get_field('date');
+            $date = DateTime::createFromFormat('Ymd', $date_field);
+            $formatted_date = $date->format('F j, Y');
+            $featured_image_id = get_post_thumbnail_id();
+            $image_src_array = wp_get_attachment_image_src($featured_image_id, 'medium');
+          ?>
 
-        <article class="news-archive__news-item">
-          <picture class="news-archive__picture">
-            <source>
-            <img src="<?php echo $image_src_array[0]; ?>" alt="News Item Featured Image" class="news-archive__image">
-          </picture>
-          <a href="<?= $link ?>" class="news-archive__link">
-            <h2 class="news-archive__title"><?php htmlentities( the_title() ); ?></h2>
-          </a>
-          <p class="news-archive__date"><?= $formatted_date ?></p>
-          <p class="news-archive__description"><?= $description ?></p>
-        </article>
+          <article class="news-archive__news-item">
+            <picture class="news-archive__picture">
+              <source>
+              <img src="<?php echo $image_src_array[0]; ?>" alt="News Item Featured Image" class="news-archive__image">
+            </picture>
+            <a href="<?= $link ?>" class="news-archive__link">
+              <h2 class="news-archive__title"><?php htmlentities( the_title() ); ?></h2>
+            </a>
+            <p class="news-archive__date"><?= $formatted_date ?></p>
+            <p class="news-archive__description"><?= $description ?></p>
+          </article>
 
-      <?php endwhile; ?>
-    <?php else : ?>
-      <h1>There are currently no news items to display!</h1>
-    <?php endif; ?>
+        <?php endwhile; ?>
+      <?php else : ?>
+        <h1>There are currently no news items to display!</h1>
+      <?php endif; ?>
+    </div>
   </div>
 </div>
 <?php get_footer(); ?>
